3858e8e41dd3cb579a9a0bf243e838d72feb68c6
XROLE <xrolediamond@gmail.com>
2018-08-20 16:50:37 -0700

    solved factorize-a-number

--- algo.js
+++ algo.js
@@ -1,18 +1,28 @@
-// ======================================================================REVERSE A STRING
-// Reverse the provided string.
-// You may need to turn the string into an array before you can reverse it.
-// Your result must be a string.
+//   =========================================================================== FACTORIZE A NUMBER
+// Return the factorial of the provided integer.
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+// Only integers greater than or equal to zero will be supplied to the function.
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
-// ================================================================    ANSWER
+// =================================================================================ANSWER
+function factorialize(num) {
+    if (num < 0){
+        return -1
+    }else if (num == 0 || num == 1){
+        return 1
+    }else{
 
-function reverseString(str) {
-    return str.split('').reverse().join('');
- }
+        return num * (factorialize(num - 1));
+    }
+  }
   
- console.log(reverseString("hello"));
-// =================================================================== TEST
-// reverseString("hello") should return a string.
-// reverseString("hello") should become "olleh".
-// reverseString("Howdy") should become "ydwoH".
-// reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+  console.log(factorialize(5));
+
+// ================================================================================TEST
+// factorialize(5) should return a number.
+// factorialize(5) should return 120.
+// factorialize(10) should return 3628800.
+// factorialize(20) should return 2432902008176640000.
+// factorialize(0) should return 1.
