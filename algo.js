25a866bf137db02ddc82050aac26682b3e4cec4e
XROLE <xrolediamond@gmail.com>
2018-08-20 23:44:41 -0700

    error checking

--- algo.js
+++ algo.js
@@ -1,25 +1,27 @@
-//==================================================================== FIND THE LONGEST WORD IN A STRING
-// Return the length of the longest word in the provided sentence.
-// Your response should be a number.
+//==================================================================== RETURN THE LARGEST OF FOUR ARRAY
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 //=============================================================================== ANSWER
-function findLongestWordLength(str) {
-    let string = str.split(' ');
-    let longestWord = 0;
-    for(let i = 0; i < string.length; i++){
-        if(string[i].length > longestWord){
-            longestWord = string[i].length;
-        }
+function largestOfFour(arr) {
+    let newArray = [];
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++){
+    //     for(let j = 0; j = arr[i].length; j++){
+    //         if(arr[i][j].length > counter){
+    //             counter = arr[i][j].length;
+    //         }
+    //     }
+    //   newArray.push(counter);
+    return arr[i]
     }
-    return longestWord;
-}
+    // return newArray;
+  }
   
-  console.log('length fo longest word: ', (findLongestWordLength("What if we try a super-long word such as otorhinolaryngology")));
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 // =============================================================================== TEST
-// findLongestWordLength("The quick brown fox jumped over the lazy dog") should return a number.
-// findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6.
-// findLongestWordLength("May the force be with you") should return 5.
-// findLongestWordLength("Google do a barrel roll") should return 6.
-// findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8.
-// findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") should return 19.
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array.
+// largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27, 5, 39, 1001].
+// largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000].
+// largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) should return [25, 48, 21, -3].
