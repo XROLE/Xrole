5cf6a8dc414f2ccc30b0e4efc76c9f30bf53e6ce
XROLE <xrolediamond@gmail.com>
2018-08-20 23:25:14 -0700

    solve Find-the-Longest-Word-in-a-String

--- algo.js
+++ algo.js
@@ -1,28 +1,25 @@
-//   =========================================================================== FACTORIZE A NUMBER
-// Return the factorial of the provided integer.
-// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
-// Factorials are often represented with the shorthand notation n!
-// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
-// Only integers greater than or equal to zero will be supplied to the function.
+//==================================================================== FIND THE LONGEST WORD IN A STRING
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
-// =================================================================================ANSWER
-function factorialize(num) {
-    if (num < 0){
-        return -1
-    }else if (num == 0 || num == 1){
-        return 1
-    }else{
-
-        return num * (factorialize(num - 1));
+//=============================================================================== ANSWER
+function findLongestWordLength(str) {
+    let string = str.split(' ');
+    let longestWord = 0;
+    for(let i = 0; i < string.length; i++){
+        if(string[i].length > longestWord){
+            longestWord = string[i].length;
+        }
     }
-  }
+    return longestWord;
+}
   
-  console.log(factorialize(5));
-
-// ================================================================================TEST
-// factorialize(5) should return a number.
-// factorialize(5) should return 120.
-// factorialize(10) should return 3628800.
-// factorialize(20) should return 2432902008176640000.
-// factorialize(0) should return 1.
+  console.log('length fo longest word: ', (findLongestWordLength("What if we try a super-long word such as otorhinolaryngology")));
+// =============================================================================== TEST
+// findLongestWordLength("The quick brown fox jumped over the lazy dog") should return a number.
+// findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6.
+// findLongestWordLength("May the force be with you") should return 5.
+// findLongestWordLength("Google do a barrel roll") should return 6.
+// findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8.
+// findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") should return 19.
