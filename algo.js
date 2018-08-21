ebce66bbf19e3a749d6ce4b79855a896f63f4495
XROLE <xrolediamond@gmail.com>
2018-08-21 07:22:34 -0700

    solved boo whoo

--- algo.js
+++ algo.js
@@ -1,13 +1,22 @@
-//==================================================================== FINDERS KEEPERS
-// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+//==================================================================== BOO WHOO
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
 // Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
 //=============================================================================== ANSWER
-function findElement(arr, func) {
-  let num = arr.filter(func);
-  return num[0];
+function booWho(bool) {
+ 
+  return  typeof bool === 'boolean';
 }
 
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+console.log('result: ',booWho(true));
 // =============================================================================== TEST
-// findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) should return 8.
-// findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) should return undefined.
+// booWho(true) should return true.
+// booWho(false) should return true.
+// booWho([1, 2, 3]) should return false.
+// booWho([].slice) should return false.
+// booWho({ "a": 1 }) should return false.
+// booWho(1) should return false.
+// booWho(NaN) should return false.
+// booWho("a") should return false.
+// booWho("true") should return false.
+// booWho("false") should return false.
