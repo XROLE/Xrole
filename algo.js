1c8bca5f73cbe4aa0344fbcf4ba05302070f2587
XROLE <xrolediamond@gmail.com>
2018-08-21 07:12:41 -0700

    solve finders keepers

--- algo.js
+++ algo.js
@@ -1,17 +1,13 @@
-//==================================================================== TRUNCATING A STRING
-// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
-// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+//==================================================================== FINDERS KEEPERS
+// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
 //=============================================================================== ANSWER
-function truncateString(str, num) {
-    // PLEASE SOLVE IT YOURSELF
-    return str;
-  }
-  
-  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+function findElement(arr, func) {
+  let num = arr.filter(func);
+  return num[0];
+}
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
 // =============================================================================== TEST
-// truncateString("A-tisket a-tasket A green and yellow basket", 8) should return "A-tisket...".
-// truncateString("Peter Piper picked a peck of pickled peppers", 11) should return "Peter Piper...".
-// truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length) should return "A-tisket a-tasket A green and yellow basket".
-// truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) should return "A-tisket a-tasket A green and yellow basket".
-// truncateString("A-", 1) should return "A...".
-// truncateString("Absolutely Longer", 2) should return "Ab...".
+// findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) should return 8.
+// findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) should return undefined.
