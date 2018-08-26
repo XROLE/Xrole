e7af011bc6f20826f238f89c993c8e5a0e2faf3b
XROLE <xrolediamond@gmail.com>
2018-08-25 21:36:58 -0700

    solved the difference between two arrays

--- algo2.js
+++ algo2.js
@@ -1,22 +1,41 @@
-//==================================================================== SUM ALL NUMBERS IN A RANGE
-// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
-// The lowest number will not always come first.
+//==================================================================== DIFF TWO ARRAYS
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 // Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+// Note
+// You can return the array with its elements in any order
 //=============================================================================== ANSWER
-function sumAll(arr) {
-    let finalArr = [];
-    let newArr = arr.sort((a, b) => a - b);
-    for (let i = newArr[0]; i <= newArr[1]; i++){
-        finalArr.push(i);
+function diffArray(arr1, arr2) {
+    var newArr = [];
+    // Same, same; but different.
+    for(let i = 0; i <arr2.length; i++){
+        if(!arr1.includes(arr2[i])){
+            newArr.push(arr2[i])
+        }
     }
-    return finalArr.reduce((x, y) => x + y);
+    for(let i = 0; i <arr1.length; i++){
+        if(!arr2.includes(arr1[i])){
+            newArr.push(arr1[i])
+        }
+    }
+    
+    return newArr;
   }
   
-  console.log('>>>>>', sumAll([4, 3]));
+  console.log('>>>>>>>>',diffArray([1, 2, 3, 5, 9], [1, 2, 3, 4, 5]));
   
   // =============================================================================== TEST
-// sumAll([1, 4]) should return a number.
-// sumAll([1, 4]) should return 10.
-// sumAll([4, 1]) should return 10.
-// sumAll([5, 10]) should return 45.
-// sumAll([10, 5]) should return 45.
+//   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+//   ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+//   ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with one item.
+//   ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+//   ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with two items.
+//   ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+//   ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return an empty array.
+//   [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+//   [1, 2, 3, 5], [1, 2, 3, 4, 5] should return an array with one item.
+//   [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+//   [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return an array with two items.
+//   [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+//   [], ["snuffleupagus", "cookie monster", "elmo"] should return an array with three items.
+//   [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+//   [1, "calf", 3, "piglet"], [7, "filly"] should return an array with six items.
