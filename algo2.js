4235c4f16b0fa09e7e59fcb30b75179225bd78c6
XROLE <xrolediamond@gmail.com>
2018-08-27 15:40:49 -0700

    NOT SOLVED Coppied solution from born fire

--- algo2.js
+++ algo2.js
@@ -1,22 +1,33 @@
 //==================================================================== SPINAL TAP CASE
-// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
-// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code
-//=============================================================================== ANSWER
-function spinalCase(str) {
-  // Create a variable for the white space and underscores.
-  var regex = /\s+|_+/g;
+// Translate the provided string to pig latin.
+// Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+// If a word begins with a vowel you just add "way" to the end.
+// Input strings are guaranteed to be English words in all lowercase.
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+// //=============================================================================== ANSWER
+function translate(str) {
+  // Create variables to be used
+  var pigLatin = '';
+  var regex = /[aeiou]/gi;
+  // Check if the first character is a vowel
+  if (str[0].match(regex)) {
+    pigLatin = str + 'way';
+  } else {
+    // Find how many consonants before the firs vowel.
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+    // Take the string from the first vowel to the last char
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
+  }
 
-  // Replace low-upper case to low-space-uppercase
-  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
-
-  // Replace space and underscore with -
-  return str.replace(regex, '-').toLowerCase();
+  return pigLatin;
 }
 
-console.log('>>>>>', spinalCase('Teletubbies say Eh-oh'));
-
 // =============================================================================== TEST
-// spinalCase("This Is Spinal Tap") should return "this-is-spinal-tap".
-// spinalCase("thisIsSpinalTap") should return "this-is-spinal-tap".
-// spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
-// spinalCase("Teletubbies say Eh-oh") should return "teletubbies-say-eh-oh".
+// translatePigLatin("california") should return "aliforniacay".
+// translatePigLatin("paragraphs") should return "aragraphspay".
+// translatePigLatin("glove") should return "oveglay".
+// translatePigLatin("algorithm") should return "algorithmway".
+// translatePigLatin("eight") should return "eightway".
+// Should handle words where the first vowel comes in the end of the word.
+// Should handle words without vowels.
