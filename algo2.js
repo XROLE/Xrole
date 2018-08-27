69210d1c8181b21c2d30ab23ab45279aca5dce0f
XROLE <xrolediamond@gmail.com>
2018-08-27 15:07:39 -0700

    refactored spinal tap case

--- algo2.js
+++ algo2.js
@@ -3,14 +3,14 @@
 // Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code
 //=============================================================================== ANSWER
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  const strArray = str.toLowerCase().split(' ');
-  let  newString = strArray[0];
-  for(let i =1; i <strArray.length; i++){
-    newString += `-${strArray[i]}`;
-  }
-   return newString;
+  // Create a variable for the white space and underscores.
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  // Replace space and underscore with -
+  return str.replace(regex, '-').toLowerCase();
 }
 
 console.log('>>>>>', spinalCase('Teletubbies say Eh-oh'));
