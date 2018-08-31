93e8e4f7fe0aef20c791eb486cefc73fca8f7139
XROLE <xrolediamond@gmail.com>
2018-08-31 16:42:50 -0700

    soleve mySort challenge

--- algo3.js
+++ algo3.js
@@ -1,66 +1,29 @@
-// ========================================== NINJA'S RERTURN
-// Write a function which will take two integers X, Y arguments and will return:
-    // If X is greater than Y return even numbers between X and Y.
-    // When X is less that Y, return odd numbers between X and Y
-    // When X and wy are both odd numbers, return multiples of A between X and Y
-    // find the largest number
-    // find the smallest number
-    // When they are both even numbers, return the average value
-
+// ========================================== MY SOSRT
+// Write a mySort function which takes in an array integers, and should return an array of the inputed integers sorted such that the odd numbers come first and even numbers come last.
+    // For exampl1e:
+    // mySort( [90, 45, 66, 'bye', 100.5] )
+    // should return
+    // [45, 66, 90, 100]
     // =============================================== ANSWERS
-    // function ninjRetrun(x, y){
-    //     let ans = [];
-    //     if(x > y){
-    //         for(let i = y + 1; i < x; i++){
-    //            if(i % 2 === 0){
-    //                ans.push(i);
-    //            }
-    //         }
-    //         return ans;
-    //     }
-    //         for (let i = x; i < y; i++){
-    //             if(i % 2 !== 0){
-    //                 ans.push(i)
-    //             }
-    //         }
-    //     return ans;
-    // }
-    // console.log('>>>>',ninjRetrun(5, 20));
-    let removeDuplicates = (word) => { 
-        let result = {}; 
-        let uniq = [];
-        let uniques = [];
-        let duplicates = [];
-        let spChar =[];
-        let newStr = word.toLowerCase();
-        let strAlph = "abcdefghijklmnopqrstuvwxyz";
-        
-        for (let i = 0; i < newStr.length; i++) { 
-            if (!strAlph.includes(newStr.charAt(i))) { 
-                spChar.push(newStr.charAt(i));
-            } else {
-                uniq.push(newStr.charAt(i));
+    function mySort(num) { 
+        let odd = [];
+        let even = [];
+        let result = [];        
+        if (!Array.isArray(num)) { 
+            return "Please input an array";
+        } else { 
+            for (let counter = 0; counter < num.length; counter++) {
+                if (typeof num[counter] === 'string') {
+                    num.splice(counter, 0);
+                } else if (Math.floor(num[counter]) % 2 === 0) {
+                    even.push(Math.floor(num[counter]));
+                } else {
+                    odd.push(num[counter]);
+                }
             }
         }
         
-        let letters = uniq.sort().join('');
-        let count = {};
-        let letter;
-        for (let j = 0; j < letters.length; j++) { 
-            letter = letters.charAt(j);
-            
-            if(count[letter]) {
-                count[letter] = 1 + count[letter];
-            } else {
-                uniques.push(letter);
-                count[letter] = 1;
-            }
-            
-            if (count[letter] > 1) {
-                duplicates.push(letter)
-            }
-        }
-        result['uniques'] = uniques.join('');
-        result['duplicates'] = duplicates.length;
-        return result;
+        let newOdd = odd.sort(function(a,b) {return a-b}); 
+        let newEven = even.sort(function(a,b) {return a-b}); 
+        return result = newOdd.concat(newEven);       
     }
