dcf849a5d639106b2af0f9b13883bf5da6678b76
XROLE <xrolediamond@gmail.com>
2018-08-31 16:26:07 -0700

    solved removed duplicate completely

--- algo3.js
+++ algo3.js
@@ -8,21 +8,59 @@
     // When they are both even numbers, return the average value
 
     // =============================================== ANSWERS
-    function ninjRetrun(x, y){
-        let ans = [];
-        if(x > y){
-            for(let i = y + 1; i < x; i++){
-               if(i % 2 === 0){
-                   ans.push(i);
-               }
+    // function ninjRetrun(x, y){
+    //     let ans = [];
+    //     if(x > y){
+    //         for(let i = y + 1; i < x; i++){
+    //            if(i % 2 === 0){
+    //                ans.push(i);
+    //            }
+    //         }
+    //         return ans;
+    //     }
+    //         for (let i = x; i < y; i++){
+    //             if(i % 2 !== 0){
+    //                 ans.push(i)
+    //             }
+    //         }
+    //     return ans;
+    // }
+    // console.log('>>>>',ninjRetrun(5, 20));
+    let removeDuplicates = (word) => { 
+        let result = {}; 
+        let uniq = [];
+        let uniques = [];
+        let duplicates = [];
+        let spChar =[];
+        let newStr = word.toLowerCase();
+        let strAlph = "abcdefghijklmnopqrstuvwxyz";
+        
+        for (let i = 0; i < newStr.length; i++) { 
+            if (!strAlph.includes(newStr.charAt(i))) { 
+                spChar.push(newStr.charAt(i));
+            } else {
+                uniq.push(newStr.charAt(i));
             }
-            return ans;
         }
-            for (let i = x; i < y; i++){
-                if(i % 2 !== 0){
-                    ans.push(i)
-                }
+        
+        let letters = uniq.sort().join('');
+        let count = {};
+        let letter;
+        for (let j = 0; j < letters.length; j++) { 
+            letter = letters.charAt(j);
+            
+            if(count[letter]) {
+                count[letter] = 1 + count[letter];
+            } else {
+                uniques.push(letter);
+                count[letter] = 1;
             }
-        return ans;
+            
+            if (count[letter] > 1) {
+                duplicates.push(letter)
+            }
+        }
+        result['uniques'] = uniques.join('');
+        result['duplicates'] = duplicates.length;
+        return result;
     }
-    console.log('>>>>',ninjRetrun(5, 20));
