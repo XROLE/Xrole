4afccf76ccc275836e0a2d936681ccfb06a0a580
XROLE <xrolediamond@gmail.com>
2018-08-31 17:08:40 -0700

    solve power challenge

--- algo3.js
+++ algo3.js
@@ -1,29 +1,17 @@
-// ========================================== MY SOSRT
-// Write a mySort function which takes in an array integers, and should return an array of the inputed integers sorted such that the odd numbers come first and even numbers come last.
-    // For exampl1e:
-    // mySort( [90, 45, 66, 'bye', 100.5] )
-    // should return
-    // [45, 66, 90, 100]
+// ========================================== POWER RECURSION
+// Write a function named power that accepts two arguments, a and b and calculates a raised to the power b.
+// Example
+// power(2, 3) => 8
+// Note: Don't use
+// 2 ** 3
+// and don't use
+// Math.pow(2, 3)
     // =============================================== ANSWERS
-    function mySort(num) { 
-        let odd = [];
-        let even = [];
-        let result = [];        
-        if (!Array.isArray(num)) { 
-            return "Please input an array";
-        } else { 
-            for (let counter = 0; counter < num.length; counter++) {
-                if (typeof num[counter] === 'string') {
-                    num.splice(counter, 0);
-                } else if (Math.floor(num[counter]) % 2 === 0) {
-                    even.push(Math.floor(num[counter]));
-                } else {
-                    odd.push(num[counter]);
-                }
-            }
+    let power = (a,b) => { 
+        if (b <= 1) {
+            return a;
+        } else {
+            return a * power(a, b-1); 
         }
-        
-        let newOdd = odd.sort(function(a,b) {return a-b}); 
-        let newEven = even.sort(function(a,b) {return a-b}); 
-        return result = newOdd.concat(newEven);       
-    }
+    };
+    console.log(power(3, 4));
