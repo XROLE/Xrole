8d00027aba168eed1e4385b3c19ba621ab3aaedd
XROLE <xrolediamond@gmail.com>
2018-10-03 15:01:34 -0700

    solved successfully

--- algo3.js
+++ algo3.js
@@ -1,41 +1,10 @@
-// function resolveAfter2Seconds() {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve('resolved');
-//     }, 2000);
-//   });
-// }
-// async  function sayHello(){
-// console.log('  calling...');
-// const result = await resolveAfter2Seconds();
-// console.log(result);
-// console.log('  done');
-// };
-
-// sayHello();
-
-// function delay(){
-//   return new Promise((resolve) => setTimeout(() => resolve('This one is from delay')));
-// }
-
-// async function displayDelay(){
-//  console.log('no delay');
-//  const result = await delay();
-//  console.log(result);
-//   console.log('Most delay');
-// }
-
-// displayDelay();
-
-function multply(a, b){
-
-  return new Promise((resolve) => resolve('I am a chosen one '));
+function sumAll(arr) {
+  let sortedArray = arr.sort((a, b) => a - b);
+let sum = 0;
+  for(let x = sortedArray[0]; x <= sortedArray[1]; x++){
+    sum += x;
+  }
+    return sum;
 }
 
-async function doubleMultiply(){
-  console.log('  I am a chosen one');
-  const result = await multply(2,9);
-  console.log(' ', result);
-  console.log('  Me and my family shall make heaven at last in Jesus name. Amen');
-}
-doubleMultiply();
+console.log('   ', sumAll([1, 4]));
